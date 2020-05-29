c40d9493cfcc223e58a5725dccca2d814481fba3
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-29 23:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,4 +57,10 @@ if (item != cart[item]) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (typeof(cardNumber) === "undefined") {
+    return "Sorry, we don't have a credit card on file for you." 
+  } else {
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
 }

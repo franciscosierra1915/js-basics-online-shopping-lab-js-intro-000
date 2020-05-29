42fbd9a98765f4c9bed5a0bd625743d65e86a8e8
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-29 23:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ function viewCart() {
 
 function total() {
   // write your code here
-  var total = 0; 
+  var z = 0; 
   for(var i = 0 ; i < cart.length; i++) {
-    total += cart[i].itemPrice;
+    z += cart[i].itemPrice;
   }
-  return total;
+  return z;
 }
 
 function removeFromCart(item) {

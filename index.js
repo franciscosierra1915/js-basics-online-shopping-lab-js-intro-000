47d934947895da38fd938c938c90af1b8b368f6b
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-30 00:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,11 +58,11 @@ if (item != cart[item]) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-  if (cardNumber === undefined) {
-    return "Sorry, we don't have a credit card on file for you." 
-  } else {cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  if (cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    const total1 = total()
+    cart = []
+    return `Your total cost is $${total1}, which will be charged to the card ${cardNumber}.`
   }
-
 }
